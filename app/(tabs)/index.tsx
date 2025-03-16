ba186b82f4847f433dacd90e79f3a9e572637f86
Xolani Masuku <xolanim@jendamark.co.za>
2025-03-16 20:04:36 +0200

    added a useFetch custom hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,15 @@ import {images} from "@/constants/images";
 import {icons} from "@/constants/icons";
 import  SearchBar from "@/components/SearchBar";
 import {useRouter} from "expo-router";
+import {fetchMovies} from "@/services/api";
+import  useFetch from "@/services/usefetch"
 
 const Index = () => {
     const router = useRouter();
+    const {data: movies,
+        loading: moviesLoading,
+        error: moviesError,
+    } = useFetch(() => fetchMovies({ query: ''}));
     return (
         <View className="flex-1  bg-primary">
             <Image source={images.bg} className="absolute w-full z-0"/>
@@ -15,7 +21,7 @@ const Index = () => {
                 <Image source={icons.logo} className="w-12 h10 mt-20 mb-5 mx-auto"/>
                 <View>
                     <SearchBar
-                        onPress={() => router.push("//searchh")}
+                        onPress={() => router.push("/search")}
                         placeholder="Search for a movie"
                     />
                 </View>
