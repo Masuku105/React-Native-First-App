0f75c57042cc0ee9a98973de67deb8744ab46aec
Xolani Masuku <xolanim@jendamark.co.za>
2025-04-05 16:30:04 +0200

    Added movies card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,10 +1,12 @@
-import {TouchableOpacity, Image, Text} from 'react-native'
+import {TouchableOpacity, Image, Text, View} from 'react-native'
 import React from 'react'
 import {Link} from "expo-router";
+import {icons} from "@/constants/icons";
+
 
 const MovieCard = ({id, poster_path, title, release_date}:Movie) => {
     return (
-        <Link href={`/movie/${id}`} className="w-1/3" asChild>
+        <Link href={`/movies/${id}`} className="w-1/3" asChild>
             <TouchableOpacity className="w-[30%]">
                 <Image
                     source={{uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : 'https://placehold.co/600x400/lalala/ffffff.png'}}
@@ -14,6 +16,11 @@ const MovieCard = ({id, poster_path, title, release_date}:Movie) => {
                 <Text className="tet-sm font-bold text-white">
                     {title}
                 </Text>
+                <View className="flex-row items-center justify-start gap-x-1">
+                    <Image src={icons.star} className="size-4"/>
+
+
+                </View>
 
             </TouchableOpacity>
         </Link>
